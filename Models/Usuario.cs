13e2b263aa7c4fab3f685f66da2f2c5510adb03e
NadineLewit <114581330+NadineLewit@users.noreply.github.com>
2022-11-22 21:08:45 -0300

    Add files via upload

---
+++ Models/Usuario.cs
@@ -0,0 +1,18 @@
+namespace TP08;
+ 
+public class Usuario
+{
+    private int _idUsuario;
+    private string _nombre;
+ 
+    public int IdUsuario
+    {
+        get{return _idUsuario;}
+        set{_idUsuario= value;}
+    }
+    public string Nombre
+    {
+        get{return _nombre;}
+        set{_nombre= value;}
+    }
+}
